fc4b58135e7f90d6857e698c9dc91420d5a4450b
SanHub-Soln <heresanjay08@gmail.com>
2026-01-03 23:05:17 +0530

    seed.js is added
    here the the data of the videos are can fetch and updated here

---
+++ src/data/seed.js
@@ -0,0 +1,39 @@
+// src/data/seed.js
+
+const seedVideos = [
+  {
+    id: "seed-1",
+    title: "India vs Australia Final",
+    teams: "India vs Australia",
+    sport: "Cricket",
+    league: "ICC",
+    type: "live",
+    length: "2h 45m",
+    video: "https://www.w3schools.com/html/mov_bbb.mp4",
+    poster: "https://picsum.photos/500/300?random=101",
+  },
+  {
+    id: "seed-2",
+    title: "Manchester United vs Arsenal",
+    teams: "Man United vs Arsenal",
+    sport: "Football",
+    league: "EPL",
+    type: "match",
+    length: "1h 55m",
+    video: "https://www.w3schools.com/html/mov_bbb.mp4",
+    poster: "https://picsum.photos/500/300?random=102",
+  },
+  {
+    id: "seed-3",
+    title: "Djokovic vs Nadal Highlights",
+    teams: "Djokovic vs Nadal",
+    sport: "Tennis",
+    league: "ATP",
+    type: "match",
+    length: "18m",
+    video: "https://www.w3schools.com/html/mov_bbb.mp4",
+    poster: "https://picsum.photos/500/300?random=103",
+  },
+];
+
+export default seedVideos;
